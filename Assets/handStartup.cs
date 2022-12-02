c4a7b0a54e4ae66187036efba540c1da2a023a3c
CosmicNyx <54959623+CosmicNyx@users.noreply.github.com>
2022-12-01 20:27:47 -0500

    different avatars

--- Assets/handStartup.cs
+++ Assets/handStartup.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class handStartup : MonoBehaviour
 {
+
+    public grabClientID clientID;
+
     // Start is called before the first frame update
     void Start()
     {
-        foreach(Transform child in transform)
+
+        //Transform leftHand = transform.Find("transfromAlignLeft");
+
+        //-----------------------
+        foreach (Transform child in transform)
         {
-            child.gameObject.SetActive(false);
+            if (clientID.playerID == 0 && child.gameObject.name == "hand1")
+            {
+                child.gameObject.SetActive(true);
+                //leftHand.gameObject.SetActive(false);
+            }
+            else if (clientID.playerID == 1 && child.gameObject.name == "hand2")
+            {
+                child.gameObject.SetActive(true);
+            }
+            else
+            {
+                child.gameObject.SetActive(false);
+            }
+
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
